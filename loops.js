1a1bc8f8dc624c4dc75fad7d2eb64596332434cb
Erick Villeda <eeverickeev@gmail.com>
2020-07-27 00:34:44 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -16,7 +16,7 @@ function whileLoop(n){
   return(`done`);
 }
 function doWhileLoop(num){
- function incrementVariable(i){}
+ function incrementVariable(){}
   do{
   console.log(`I run once regardless.`)
   } while (incrementVariable() < num)
